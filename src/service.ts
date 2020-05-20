af95b0a9bddcc9734dc888e3dbe969b388a2bf5e
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2020-05-20 09:20:25 +0200

    Use active state provided in request for newly cretated users

--- src/service.ts
+++ src/service.ts
@@ -234,7 +234,6 @@ export class UserService extends ServiceBase {
     for (let i = 0; i < usersList.length; i++) {
       let user: User = usersList[i];
       user.activation_code = '';
-      user.active = user.active;
       user.unauthenticated = false;
       if (user.invite) {
         user.active = false;
