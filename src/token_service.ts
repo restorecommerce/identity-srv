3da9aaa2b818c05f3522004e872fcee403424477
akumar <arun.kumar@n-fuse.co>
2020-09-30 17:41:52 +0200

    fix to use passed subject from request

--- src/token_service.ts
+++ src/token_service.ts
@@ -1,7 +1,7 @@
 import { Logger, errors } from '@restorecommerce/chassis-srv';
 import { ACSAuthZ, PermissionDenied, AuthZAction, Decision, Subject, ApiKey } from '@restorecommerce/acs-client';
 import { AccessResponse, getSubjectFromRedis, checkAccessRequest } from './utils';
-import { RedisClient } from 'redis';
+import { RedisClient, createClient } from 'redis';
 import * as _ from 'lodash';
 import { UserService } from './service';
 import * as uuid from 'uuid';
@@ -45,15 +45,19 @@ export class TokenService {
   logger: Logger;
   cfg: any;
   authZ: ACSAuthZ;
-  redisClient: RedisClient;
+  tokenRedisClient: RedisClient;
+  subjectRedisClient: RedisClient;
   userService: UserService;
   constructor(cfg: any, logger: any, authZ: ACSAuthZ, tokenRedisClient: RedisClient,
     userService: UserService) {
     this.logger = logger;
     this.authZ = authZ;
     this.cfg = cfg;
-    this.redisClient = tokenRedisClient;
+    this.tokenRedisClient = tokenRedisClient;
     this.userService = userService;
+    const redisConfig = cfg.get('redis');
+    redisConfig.db = this.cfg.get('redis:db-indexes:db-subject');
+    this.subjectRedisClient = createClient(redisConfig);
   }
 
   private getKey(id: string) {
@@ -70,11 +74,44 @@ export class TokenService {
     }
 
     const tokenData = call.request;
+    const payload = unmarshallProtobufAny(tokenData.payload);
+    tokenData.payload = payload;
     let subject = await getSubjectFromRedis(call);
     call.request = await this.createMetadata(tokenData, subject);
+
+    // persist subject to reids - containing role_assocs
+    const key = this.getKey(tokenData.id);
+    const claims = payload.claims;
+    if (claims && claims.data && claims.data.id) {
+      let redisKey = `cache:${claims.data.id}:subject`;
+      let subject = await new Promise((resolve, reject) => {
+        this.subjectRedisClient.get(redisKey, (err, reply) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          if (reply) {
+            this.logger.debug('Found Subject key in redis', key);
+            resolve(JSON.parse(reply));
+          } else {
+            resolve();
+          }
+        });
+      });
+      if (!subject) {
+        await this.subjectRedisClient.set(redisKey,
+          JSON.stringify({
+            id: claims.data.id, role_associations: claims.data.role_associations,
+            default_scope: claims.data.default_scope, tokens: claims.data.tokens,
+            token_name: payload.claims.token_name
+          }));
+      }
+    }
+
     let acsResponse: AccessResponse;
     try {
-      acsResponse = await checkAccessRequest(subject, tokenData, AuthZAction.CREATE,
+      acsResponse = await checkAccessRequest(subject, call.request, AuthZAction.CREATE,
         'token', this);
     } catch (err) {
       this.logger.error('Error occurred requesting access-control-srv:', err);
@@ -85,18 +122,15 @@ export class TokenService {
     }
 
     if (acsResponse.decision === Decision.PERMIT) {
-      const multi = this.redisClient.multi();
-      const key = this.getKey(tokenData.id);
-      const payload = unmarshallProtobufAny(tokenData.payload);
+      const multi = this.tokenRedisClient.multi();
       tokenData.payload = JSON.stringify(payload);
-
       multi.set(key, tokenData.payload);
       if (payload.grantId) {
         const grantKey = grantKeyFor(payload.grantId);
         multi.rpush(grantKey, key);
         // if you're seeing grant key lists growing out of acceptable proportions consider using LTRIM
         // here to trim the list to an appropriate length
-        const ttl = await this.redisClient.ttl(grantKey);
+        const ttl = await this.tokenRedisClient.ttl(grantKey);
         if (tokenData.expires_in > ttl) {
           multi.expire(grantKey, tokenData.expires_in);
         }
@@ -113,7 +147,7 @@ export class TokenService {
         multi.set(uidKey, tokenData.id);
         multi.expire(uidKey, tokenData.expires_in);
       }
-      const response = await new Promise((resolve, reject) => {
+      const response: any = await new Promise((resolve, reject) => {
         multi.exec(async (err, res) => {
           if (err) {
             reject(err);
@@ -124,46 +158,48 @@ export class TokenService {
               status: `AccessToken data ${tokenData.id} persisted successfully`
             };
             this.logger.info('AccessToken data persisted successfully for subject', { id: subject.id });
-            // update user token here and also last login
-            const userData = await this.userService.find({ request: { id: payload.accountId, subject: { id: payload.accountId } } });
-            if (userData && userData.items && userData.items.length > 0) {
-              let user = userData.items[0];
-              // check if the token is existing if not update it
-              let updateToken = true;
-              let currentTokenList = [];
-              if (user && user.tokens && user.tokens.length > 0) {
-                currentTokenList = user.tokens;
-              }
-              for (let token of currentTokenList) {
-                if (token.token === payload.jti) {
-                  // token already exists and not expired
-                  updateToken = false;
-                  break;
-                }
-              }
-              let token_name;
-              if (payload.claims && payload.claims.token_name) {
-                token_name = payload.claims.token_name;
-              } else {
-                token_name =  uuid.v4().replace(/-/g, '');
-              }
-              if (updateToken) {
-                const token = {
-                  name: token_name,
-                  expires_at: payload.exp,
-                  token: payload.jti
-                };
-                currentTokenList.push(token);
-                user.tokens = currentTokenList;
-                user.last_login = new Date().getTime();
-                user.last_access = new Date().getTime();
-                await this.userService.update({ request: { items: [user], subject: { id: payload.accountId } } });
-              }
-            };
             resolve(response);
           }
         });
       });
+      if (response && response.status) {
+        // update user token here and also last login
+        const userData = await this.userService.find({ request: { id: payload.accountId, subject } });
+        if (userData && userData.items && userData.items.length > 0) {
+          let user = userData.items[0];
+          // check if the token is existing if not update it
+          let updateToken = true;
+          let currentTokenList = [];
+          if (user && user.tokens && user.tokens.length > 0) {
+            currentTokenList = user.tokens;
+          }
+          for (let token of currentTokenList) {
+            if (token.token === payload.jti) {
+              // token already exists and not expired
+              updateToken = false;
+              break;
+            }
+          }
+          let token_name, scope;
+          if (payload.claims && payload.claims.token_name) {
+            token_name = payload.claims.token_name;
+          } else {
+            token_name = uuid.v4().replace(/-/g, '');
+          }
+          if (updateToken) {
+            const token = {
+              name: token_name,
+              expires_at: payload.exp,
+              token: payload.jti
+            };
+            currentTokenList.push(token);
+            user.tokens = currentTokenList;
+            user.last_login = new Date().getTime();
+            user.last_access = new Date().getTime();
+            await this.userService.update({ request: { items: [user], subject } });
+          }
+        };
+      }
       return marshallProtobufAny(response);
     }
   }
@@ -182,7 +218,7 @@ export class TokenService {
     call.request = await this.createMetadata(call.request, subject);
     let acsResponse: AccessResponse;
     try {
-      acsResponse = await checkAccessRequest(subject, call.request, AuthZAction.READ,
+      acsResponse = await checkAccessRequest(subject, { entity: 'token' }, AuthZAction.READ,
         'token', this);
     } catch (err) {
       this.logger.error('Error occurred requesting access-control-srv:', err);
@@ -195,7 +231,7 @@ export class TokenService {
     if (acsResponse.decision === Decision.PERMIT) {
       const data: any = await new Promise((resolve, reject) => {
         const key = this.getKey(id);
-        this.redisClient.get(key, async (err, reply) => {
+        this.tokenRedisClient.get(key, async (err, reply) => {
           if (err) {
             reject(err);
             return;
@@ -249,7 +285,7 @@ export class TokenService {
     if (acsResponse.decision === Decision.PERMIT) {
       const id = await new Promise((resolve, reject) => {
         const key = uidKeyFor(uid);
-        this.redisClient.get(key, (err, reply) => {
+        this.tokenRedisClient.get(key, (err, reply) => {
           if (err) {
             reject(err);
             return;
@@ -293,7 +329,7 @@ export class TokenService {
     if (acsResponse.decision === Decision.PERMIT) {
       const id = await new Promise((resolve, reject) => {
         const key = userCodeKeyFor(call.request.user_code);
-        this.redisClient.get(key, (err, reply) => {
+        this.tokenRedisClient.get(key, (err, reply) => {
           if (err) {
             reject(err);
             return;
@@ -342,7 +378,7 @@ export class TokenService {
         // delete user token here
         if (payload && payload.value) {
           payload = unmarshallProtobufAny(payload);
-          const userData = await this.userService.find({ request: { id: payload.accountId, subject: { id: payload.accountId } } });
+          const userData = await this.userService.find({ request: { id: payload.accountId, subject } });
           if (userData && userData.items && userData.items.length > 0) {
             let user = userData.items[0];
             // check if the token is existing if not update it
@@ -362,11 +398,11 @@ export class TokenService {
               const updatedTokenList = currentTokenList.filter(token => token.token !== id);
               user.tokens = updatedTokenList;
               user.last_access = new Date().getTime();
-              await this.userService.update({ request: { items: [user], subject: { id: payload.accountId } } });
+              await this.userService.update({ request: { items: [user], subject } });
             }
           };
         }
-        this.redisClient.del(key, async (err, reply) => {
+        this.tokenRedisClient.del(key, async (err, reply) => {
           if (err) {
             reject(err);
             return;
@@ -412,10 +448,10 @@ export class TokenService {
     }
 
     if (acsResponse.decision === Decision.PERMIT) {
-      const multi = this.redisClient.multi();
+      const multi = this.tokenRedisClient.multi();
       const tokens: any = await new Promise((resolve, reject) => {
         const key = grantKeyFor(grant_id);
-        this.redisClient.lrange(key, 0, -1, (err, res) => {
+        this.tokenRedisClient.lrange(key, 0, -1, (err, res) => {
           if (err) {
             reject(err);
             return;
@@ -452,7 +488,7 @@ export class TokenService {
       throw new errors.InvalidArgument('ID was not provided for consume operation');
     }
     try {
-      this.redisClient.set(this.getKey(call.request.id), 'consumed', Math.floor(Date.now() / 1000));
+      this.tokenRedisClient.set(this.getKey(call.request.id), 'consumed', Math.floor(Date.now() / 1000));
       return marshallProtobufAny({ response: `AccessToken with ID ${call.request.id} consumed` });
     } catch (err) {
       return marshallProtobufAny({ response: `error consuming access token` });
