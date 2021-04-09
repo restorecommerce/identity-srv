e30f42662359b99df5abb323e0ba28ff6c868176
Radu1990 <et2323@gmail.com>
2021-04-09 15:45:53 +0200

    remove Dockerfile(stretch image)

--- buildImage.bash
+++ buildImage.bash
@@ -1,1 +1,10 @@
-docker build -t restorecommerce/identity-srv .
+#!/bin/bash
+
+SERVICE_NAME="identity-srv"
+
+DOCKER_BUILDKIT=1 docker build \
+  --tag restorecommerce/$SERVICE_NAME \
+  -f ./Dockerfile \
+  --cache-from restorecommerce/$SERVICE_NAME \
+  --build-arg APP_HOME=/home/node/$SERVICE_NAME \
+  .
