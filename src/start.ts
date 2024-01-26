8a138b55927e6523849d697e8d1cef2d9d67e0a4
Vilsol <me@vil.so>
2024-01-26 20:33:38 +0200

    feat: switch to bundled esbuild deployment

--- src/start.ts
+++
@@ -1,9 +0,0 @@
-import Cluster from '@restorecommerce/cluster-service';
-import { createServiceConfig } from '@restorecommerce/service-config';
-
-const cfg = createServiceConfig(process.cwd());
-const server = new Cluster(cfg);
-server.run('./lib/worker');
-process.on('SIGINT', () => {
-  server.stop();
-});
